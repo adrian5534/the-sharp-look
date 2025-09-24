0ddd186fba56b8ea7edd2a3c571bb5f00520ff8f
adrian5534 <81486606+adrian5534@users.noreply.github.com>
2025-09-24 03:11:15 -0400

    logo update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { FiScissors, FiCalendar } from 'react-icons/fi'
+import { FiCalendar } from 'react-icons/fi'
 import '../styles/navbar.css'
 
 export default function Navbar() {
@@ -8,7 +8,18 @@ export default function Navbar() {
         <div className="container">
           {/* Brand */}
           <a className="navbar-brand d-inline-flex align-items-center gap-2" href="/">
-            <span className="brand-icon" aria-hidden="true"><FiScissors /></span>
+            <span className="brand-icon">
+              {/* Decorative logo image; brand text provides the accessible name */}
+              <img
+                className="brand-logo"
+                src="/images/logo.png"
+                alt=""
+                width={28}
+                height={28}
+                loading="eager"
+                decoding="async"
+              />
+            </span>
             <span className="brand-name">The Sharp Look</span>
           </a>
 
@@ -27,7 +38,6 @@ export default function Navbar() {
 
           {/* Nav + CTA */}
           <div className="collapse navbar-collapse" id="siteNav">
-            {/* Centered links on lg+ */}
             <ul className="navbar-nav mx-lg-auto gap-lg-4 mt-3 mt-lg-0">
               <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
               <li className="nav-item"><a className="nav-link" href="/services">Services</a></li>
@@ -36,7 +46,6 @@ export default function Navbar() {
               <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
             </ul>
 
-            {/* Custom CTA (not Bootstrap .btn) */}
             <div className="ms-lg-3">
               <a href="/booking" className="nav-cta chip w-100 w-lg-auto d-inline-flex align-items-center justify-content-center gap-2">
                 <FiCalendar aria-hidden="true" /> <span>Book Now</span>
